8a5db8b1bba1a3a0dbbcc4b5b0076a08b2892c2e
Evan Tan <36868915+evantancy@users.noreply.github.com>
2025-02-20 15:52:43 +0800

    feat: add treesitter-context and configure more LSPs

--- init.lua
+++ init.lua
@@ -944,6 +944,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prismals',
       })
 
       -- Function to check if "ruff" command is available
@@ -1266,10 +1267,36 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-context',
+      config = function()
+        local treesitter_context = require 'treesitter-context'
+        treesitter_context.setup {
+          multiwindow = true, -- Enable multiwindow support.
+        }
+      end,
+    },
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'python',
+        'go',
+        'gomod',
+        'gosum',
+        'prisma',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
