0d28482312cb46e4a912c732cb869a0e75db85b8
Evan Tan <36868915+evantancy@users.noreply.github.com>
2025-03-05 14:20:32 +0800

    chore: remove comment plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -17,13 +17,6 @@ return {
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
     end,
   },
-  -- useful when there are embedded languages in certain types of files (e.g. Vue or React)
-  { 'joosepalviste/nvim-ts-context-commentstring', lazy = true },
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    lazy = false,
-  },
   {
     'olimorris/codecompanion.nvim',
     dependencies = {
