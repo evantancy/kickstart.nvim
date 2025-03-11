b796a259e4359c4a00acf2b636ee4a37669c9291
Evan Tan <36868915+evantancy@users.noreply.github.com>
2025-03-11 21:59:39 +0800

    fix: make cmp work for cmdline

--- init.lua
+++ init.lua
@@ -1204,6 +1204,56 @@ require('lazy').setup({
       luasnip.config.setup {}
 
       local lspkind = require 'lspkind'
+      local mappings = {
+        -- Select the [n]ext item
+        -- ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        -- Select the [p]revious item
+        -- ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+        -- Select the [p]revious item
+
+        -- Accept ([y]es) the completion.
+        --  This will auto-import if your LSP supports it.
+        --  This will expand snippets if the LSP sent a snippet.
+        ['<C-y>'] = cmp.mapping(cmp.mapping.confirm { select = true }, { 'i', 'c' }),
+
+        -- If you prefer more traditional completion keymaps,
+        -- you can uncomment the following lines
+        --['<CR>'] = cmp.mapping.confirm { select = true },
+        --['<Tab>'] = cmp.mapping.select_next_item(),
+        --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+        -- Manually trigger a completion from nvim-cmp.
+        --  Generally you don't need this, because nvim-cmp will display
+        --  completions whenever it has completion options available.
+        -- ['<C-Space>'] = cmp.mapping.complete {},
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<ESC>'] = cmp.mapping(cmp.mapping.abort(), { 'i', 'c' }),
+
+        -- Think of <c-l> as moving to the right of your snippet expansion.
+        --  So if you have a snippet that's like:
+        --  function $name($args)
+        --    $body
+        --  end
+        --
+        -- <c-l> will move you to the right of each of the expansion locations.
+        -- <c-h> is similar, except moving you backwards.
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
+
+        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      }
 
       cmp.setup {
         snippet = {
@@ -1218,56 +1268,7 @@ require('lazy').setup({
         -- chosen, you will need to read `:help ins-completion`
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          -- ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-          -- Select the [p]revious item
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          -- ['<C-Space>'] = cmp.mapping.complete {},
-          -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          -- ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<ESC>'] = cmp.mapping.abort(),
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
+        mapping = cmp.mapping.preset.insert(mappings),
         sources = {
           {
             name = 'lazydev',
@@ -1327,29 +1328,26 @@ require('lazy').setup({
           documentation = cmp.config.window.bordered(),
         },
       }
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      -- cmp.setup.cmdline({ '/', '?' }, {
-      --   mapping = cmp.mapping.preset.cmdline(),
-      --   sources = {
-      --     { name = 'buffer' },
-      --   },
-      -- })
-      require('cmp').setup.cmdline('/', {
+
+      cmp.setup.cmdline({ '/', '?' }, {
         sources = cmp.config.sources({
           { name = 'nvim_lsp_document_symbol' },
         }, {
           { name = 'buffer' },
         }),
+        mapping = cmp.mapping.preset.cmdline(mappings),
       })
+
       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = 'path' },
         }, {
           { name = 'cmdline' },
         }),
+        ---@diagnostic disable-next-line: missing-fields
         matching = { disallow_symbol_nonprefix_matching = false },
+        mapping = cmp.mapping.preset.cmdline(mappings),
       })
     end,
   },
