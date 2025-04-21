990dc2f3a9aacecda3148cb9ea4c8ebe5e41ee6e
Evan Tan <36868915+evantancy@users.noreply.github.com>
2025-04-22 04:04:42 +0800

    chore: minimal config asf

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,42 @@
+return {
+    {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons", optional = true },
+    config = function()
+      local fzf = require("fzf-lua")
+      fzf.setup({ "fzf-native" })
+      fzf.register_ui_select()
+      local actions = require("fzf-lua.actions")
+
+      vim.keymap.set('n', '<leader>/', fzf.live_grep, { silent = true, desc = 'Search with Rg' })
+      vim.keymap.set('n', '<leader>sb', fzf.buffers,   { silent = true, desc = 'Search Buffers' })
+      vim.keymap.set('n', '<leader>sf', fzf.files,     { silent = true, desc = 'Search Files' })
+      vim.keymap.set('n', '<leader>sR', fzf.oldfiles,  { silent = true, desc = 'Search MRU' })
+      vim.keymap.set('n', '<leader>sr', fzf.resume,    { silent = true, desc = 'Resume' })
+
+      local keyset={}
+      local n=0
+
+      for k,v in pairs(fzf) do
+        if not k:match("^_") then
+          n=n+1
+          keyset[n]=k
+        end
+      end
+      table.sort(keyset)
+
+      vim.keymap.set({'n'}, '<leader>F', function()
+        fzf.fzf_exec(keyset, {
+          prompt = 'FZF...> ',
+          actions = {
+            ["default"] = function(selected, opts)
+              fzf[selected[1]]()
+            end
+          }
+        })
+      end, { silent = true, desc = 'Search ...' })
+
+    end
+}
+}
+
