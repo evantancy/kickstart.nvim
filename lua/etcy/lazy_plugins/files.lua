0006b2471d451ad1f1f9cc64a6f825980ef69096
Evan Tan <36868915+evantancy@users.noreply.github.com>
2025-05-28 17:38:21 +0800

    feat: add <esc> to oil keymaps

--- lua/etcy/lazy_plugins/files.lua
+++ lua/etcy/lazy_plugins/files.lua
@@ -28,12 +28,33 @@ return {
           -- Set to "unmodified" to only save unmodified buffers
           autosave_changes = true,
         },
+
+        keymaps = {
+          ['g?'] = { 'actions.show_help', mode = 'n' },
+          ['<CR>'] = 'actions.select',
+          ['<C-s>'] = { 'actions.select', opts = { vertical = true } },
+          ['<C-h>'] = { 'actions.select', opts = { horizontal = true } },
+          ['<C-t>'] = { 'actions.select', opts = { tab = true } },
+          ['<C-p>'] = 'actions.preview',
+          ['<C-c>'] = { 'actions.close', mode = 'n' },
+          ['<ESC>'] = { 'actions.close', mode = 'n' },
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = { 'actions.parent', mode = 'n' },
+          ['_'] = { 'actions.open_cwd', mode = 'n' },
+          ['`'] = { 'actions.cd', mode = 'n' },
+          ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+          ['gs'] = { 'actions.change_sort', mode = 'n' },
+          ['gx'] = 'actions.open_external',
+          ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+          ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+        },
       }
     end,
   },
   {
     'b0o/incline.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', 'lewis6991/gitsigns.nvim' },
+    ---@diagnostic disable-next-line: unused-local
     config = function(_, opts)
       local devicons = require 'nvim-web-devicons'
       require('incline').setup {
